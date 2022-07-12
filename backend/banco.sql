a7f6e940efb1dc80f768fc3749e109b601328385
Cesar Torbes <cesar.110205@bento.local>
2022-07-12 17:18:08 -0300

    a

--- backend/banco.sql
+++ backend/banco.sql
@@ -12,12 +12,21 @@ CREATE TABLE IF NOT EXISTS usuario (usuario_id INT NOT NULL AUTO_INCREMENT,
                                     PRIMARY KEY (usuario_id)
                                     );
 
-CREATE TABLE IF NOT EXISTS categorias (livro_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS livros (id INT NOT NULL AUTO_INCREMENT,
                                     categoria VARCHAR(100) NOT NULL,
                                     nomelivro VARCHAR(40) NOT NULL,
                                     autor VARCHAR(120) NOT NULL,
                                     preco real,
-                                    PRIMARY KEY (livro_id)
-                                    );                                   
+                                    PRIMARY KEY (id)
+                                    );                              
+                                    
+CREATE TABLE IF NOT EXISTS livros_images (
+                                    id int NOT NULL AUTO_INCREMENT,
+                                    livro_id int NOT NULL,
+                                    filename varchar(100) NOT NULL,
+                                    PRIMARY KEY (id),
+                                    CONSTRAINT fk_livros_images FOREIGN KEY (`livro_id`) REFERENCES `livros` (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 INSERT INTO usuario ('nome', 'senha', 'email', 'numero') values ('admin', 'admin123', 'admin@gmail.com', '1')
